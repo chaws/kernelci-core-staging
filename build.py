fc569534cd961c80cb3f3912f8a22173eddd97b5
Guillaume Tucker <guillaume.tucker@collabora.com>
2018-12-10 13:54:34 +0000

    Revert "Push a build-environment variable to the backend for every build (#43)"
    This reverts commit 6c74363abe85fe51c8e49d2144face8f5213301a.

--- build.py
+++ build.py
@@ -63,7 +63,6 @@ cross_compilers = {
 # Defaults
 arch = "arm"
 cross_compile = cross_compilers[arch]
-build_environment = None
 git_describe = None
 git_describe_v = None
 git_commit = None
@@ -280,7 +279,6 @@ if use_environment:
     git_branch = os.environ.get('BRANCH', git_branch)
     git_describe = os.environ.get('GIT_DESCRIBE', git_describe)
     git_describe_v = os.environ.get('GIT_DESCRIBE_VERBOSE', git_describe_v)
-    build_environment = os.environ.get('COMPILER', build_environment)
 
 cc_cmd = "gcc -v 2>&1"
 if cross_compile:
@@ -469,7 +467,6 @@ if install:
     bmeta['arch'] = "%s" %arch
     bmeta["cross_compile"] = "%s" %cross_compile
     bmeta["compiler_version"] = "%s" %gcc_version
-    bmeta["build_environment"] = "%s" % build_environment
     bmeta["git_url"] = "%s" %git_url
     bmeta["git_branch"] =  "%s" %git_branch
     bmeta["git_describe"] =  "%s" %git_describe
@@ -525,7 +522,7 @@ if install:
 
     if publish:
         publish_path = os.path.join(
-            job, git_branch, git_describe, arch, defconfig_full, build_environment)
+            job, git_branch, git_describe, arch, defconfig_full)
         bmeta['file_server_resource'] = publish_path
 
     # Create JSON format build metadata
@@ -547,7 +544,6 @@ if install:
         build_data['git_branch'] = git_branch
         build_data['defconfig'] = defconfig
         build_data['arch'] = arch
-        build_data['build_environment'] = build_environment
         if "defconfig_full" in bmeta:
             build_data['defconfig_full'] = defconfig_full
         count = 1
